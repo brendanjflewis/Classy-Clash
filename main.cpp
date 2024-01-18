05a5815dbd3f24edcc3ab2b711f2284ae058570f
brendanjlewis <brendan.j.f.lewis@gmail.com>
2024-01-18 17:54:04 -0500

    added movement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -9,15 +10,32 @@ int main()
     InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash");
 
     Texture2D map = LoadTexture("nature_tileset/classy-clash-overworld-map.png");
+    Vector2 mapPos{0.0, 0.0};
 
     SetTargetFPS(60);
+
+    float speed{4.0};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 mapPos{0.0, 0.0};
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A))
+            direction.x -= 1.0;
+        if (IsKeyDown(KEY_D))
+            direction.x += 1.0;
+        if (IsKeyDown(KEY_W))
+            direction.y -= 1.0;
+        if (IsKeyDown(KEY_S))
+            direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
         EndDrawing();
     }
