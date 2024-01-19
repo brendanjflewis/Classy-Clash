823786cf2bc0fe1f0adb69c1e114025edb3edae6
brendanjlewis <brendan.j.f.lewis@gmail.com>
2024-01-19 12:16:54 -0500

    added collision between props and knight

--- character.cpp
+++ character.cpp
@@ -57,3 +57,12 @@ void Character::undoMovement()
 {
     worldPos = worldPosLastFrame;
 }
+
+Rectangle Character::getCollisionRec()
+{
+    return Rectangle{
+        screenPos.x,
+        screenPos.y,
+        width * scale,
+        height * scale};
+}
--- main.cpp
+++ main.cpp
@@ -18,7 +18,9 @@ int main()
 
     Character knight{windowWidth, windowHeight};
 
-    Prop rock{Vector2{0.f, 0.f}, LoadTexture("nature_tileset/Rock.png")};
+    Prop props[2]{
+        Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}};
 
     while (!WindowShouldClose())
     {
@@ -30,7 +32,11 @@ int main()
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
 
-        rock.Render(knight.getWorldPos());
+        // draw the props
+        for (auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
 
         knight.tick(GetFrameTime());
         // check map bounds
@@ -39,7 +45,16 @@ int main()
             knight.getWorldPos().x + windowWidth > map.width * mapScale ||
             knight.getWorldPos().y + windowHeight > map.height * mapScale)
         {
+
             knight.undoMovement();
+        }
+        // check prop collisions
+        for (auto prop : props)
+        {
+            if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
         }
 
         EndDrawing();
